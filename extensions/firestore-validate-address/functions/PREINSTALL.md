5f9538bf876eec0f90c1104908d14403d140dbb5
Andy Hite <andrew.hite@auctane.com>
2021-09-07 16:18:16 -0500

    WIP

--- extensions/firestore-validate-address/functions/PREINSTALL.md
+++
@@ -1,3 +0,0 @@
-Use this extension to validate addresses with the ShipEngine api.
-
-This extension listens to Realtime Database for changes json objects at the path `/addresses/{addressId}/address` and writes the results to the same `addressId`
--- extensions/firestore-validate-address/functions/src/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  location: process.env.LOCATION!,
-  databaseInstance: process.env.SELECTED_DATABASE_INSTANCE!,
-  shipengineApiKey: process.env.SHIPENGINE_API_KEY!,
-  collectionPath: process.env.COLLECTION_PATH!,
-  addressKey: process.env.ADDRESS_KEY!,
-};
