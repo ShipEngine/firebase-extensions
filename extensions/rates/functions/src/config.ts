42c47bd4ac92739404be4c4b3b3576234a0a5eb9
Patrick Jones <patrick.jones@auctane.com>
2021-09-14 09:21:57 -0500

    config

---
+++ extensions/rates/functions/src/config.ts
@@ -0,0 +1,8 @@
+export default {
+  location: process.env.LOCATION!,
+  databaseInstance: process.env.SELECTED_DATABASE_INSTANCE!,
+  shipEngineApiKey: process.env.SHIPENGINE_API_KEY!,
+  collectionPath: process.env.COLLECTION_PATH!,
+  inputKey: process.env.INPUT_KEY!,
+  resultKey: process.env.RESULT_KEY!,
+};
