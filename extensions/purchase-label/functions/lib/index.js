49798b46b42cfe29722e433bbfa5f088a8f54a8b
Andy Hite <andrew.hite@auctane.com>
2021-09-14 16:38:33 -0500

    Remove unneeded files

--- extensions/purchase-label/functions/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.purchaseLabel = void 0;
-const functions = require("firebase-functions");
-exports.purchaseLabel = functions.handler.firestore.document.onWrite(async (change) => {
-    return;
-});
-//# sourceMappingURL=index.js.map
