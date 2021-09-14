4132af432d84b034323597b38c88f4b5baba173e
Patrick Jones <patrick.jones@auctane.com>
2021-09-14 09:23:24 -0500

    add getRates extension handler

--- extensions/rates/functions/src/index.ts
+++ extensions/rates/functions/src/index.ts
@@ -1,9 +1,59 @@
 import * as functions from "firebase-functions";
+import ShipEngine, {  } from 'shipengine';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+import config from './config';
+import * as logs from './logs';
+import { Params } from "./types";
 
+interface InputPayload {
+  [key: string]: any
+}
+
+// Initialize ShipEngine client
+const shipEngine = new ShipEngine(config.shipEngineApiKey);
+
+logs.init();
+
+export const getRates = functions.handler.firestore.document.onWrite(
+  async (change) => {
+    logs.start();
+
+    const data = change.after.data() as InputPayload;
+    const params = data[config.inputKey] as Params;
+
+    if (!params) {
+      logs.shipmentMissing();
+    }
+
+    // const params: Params = shipment;
+
+    let update: any;
+
+    /**
+     * Fetch Rates
+     */
+    try {
+      logs.fetchingRates();
+      
+      const result = await shipEngine.getRatesWithShipmentDetails(params);
+      update = { result };
+    } catch (error) {
+      logs.errorFetchRates(error as Error);
+      return;
+    }
+
+    logs.ratesFetched([]); // remove args
+
+    /**
+     * Update Reference
+     */
+    try {
+      logs.parentUpdating();
+      change.after.ref.update(update);
+      logs.parentUpdated()
+    } catch (err) {
+      logs.errorUpdatingParent(err as Error);
+      return;
+    }
+  }
+)
