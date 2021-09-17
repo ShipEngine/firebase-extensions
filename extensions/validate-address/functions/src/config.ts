4258367368b88b3a17b3694e4044c94e53e897ac
Patrick Jones <patrick.jones@auctane.com>
2021-09-17 08:55:33 -0500

    add validation result key to config

--- extensions/validate-address/functions/src/config.ts
+++ extensions/validate-address/functions/src/config.ts
@@ -4,4 +4,5 @@ export default {
   shipEngineApiKey: process.env.SHIPENGINE_API_KEY!,
   collectionPath: process.env.COLLECTION_PATH!,
   addressKey: process.env.ADDRESS_KEY!,
+  validationResultKey: process.env.VALIDATION_RESULT_KEY!
 };
