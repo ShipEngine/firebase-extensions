8b9c248dcff894145b1ae05b2297face483a32bf
Patrick Jones <patrick.jones@auctane.com>
2021-09-17 11:47:47 -0500

    build common lib

---
+++ libs/common/dist/utils.d.ts
@@ -0,0 +1,3 @@
+import { DocumentSnapshot } from 'firebase-functions/v1/firestore';
+export declare const handleUpdateDocument: <T>(snapshot: DocumentSnapshot, update: T) => void;
+//# sourceMappingURL=utils.d.ts.map
