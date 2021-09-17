62736c3a92ce132ee6c56adb3e04c6cb7c954d76
Patrick Jones <patrick.jones@auctane.com>
2021-09-17 02:06:51 -0500

    update config for params

--- extensions/rates/functions/src/config.ts
+++ extensions/rates/functions/src/config.ts
@@ -3,6 +3,7 @@ export default {
   databaseInstance: process.env.SELECTED_DATABASE_INSTANCE!,
   shipEngineApiKey: process.env.SHIPENGINE_API_KEY!,
   collectionPath: process.env.COLLECTION_PATH!,
-  inputKey: process.env.INPUT_KEY!,
-  resultKey: process.env.RESULT_KEY!,
+  carrierIds: process.env.CARRIER_IDS!.split(',').map(id => id.trim()),
+  shipmentKey: process.env.INPUT_KEY!,
+  ratesKey: process.env.OUTPUT_KEY!,
 };
