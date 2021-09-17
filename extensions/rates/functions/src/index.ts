e8c398e02c99361f98ca9d419662293885400769
Patrick Jones <patrick.jones@auctane.com>
2021-09-17 02:09:37 -0500

    update structure per discussion with Andy

--- extensions/rates/functions/src/index.ts
+++ extensions/rates/functions/src/index.ts
@@ -1,13 +1,12 @@
+import ShipEngine from 'shipengine';
 import * as functions from "firebase-functions";
-import ShipEngine, {  } from 'shipengine';
+import { Change } from 'firebase-functions';
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+import { camelizeKeys } from "humps";
 
+import { RequestPayload, ResponsePayload, InputPayload, UpdatePayload } from './types';
 import config from './config';
 import * as logs from './logs';
-import { Params } from "./types";
-
-interface InputPayload {
-  [key: string]: any
-}
 
 // Initialize ShipEngine client
 const shipEngine = new ShipEngine(config.shipEngineApiKey);
@@ -15,45 +14,59 @@ const shipEngine = new ShipEngine(config.shipEngineApiKey);
 logs.init();
 
 export const getRates = functions.handler.firestore.document.onWrite(
-  async (change) => {
-    logs.start();
+  async (change: Change<DocumentSnapshot>): Promise<void> => {
+    if (!change.after.exists) return; // The document is being deleted
 
-    const data = change.after.data() as InputPayload;
-    const params = data[config.inputKey] as Params;
+    let data = change.after.data() as InputPayload;
 
-    if (!params) {
-      logs.shipmentMissing();
-    }
+    // Support situations where the keys may be snake_cased
+    data = camelizeKeys(data) as InputPayload;
+    
+    logs.start(data);
 
-    // const params: Params = shipment;
+    // Build the request payload and execute the label purchase
+    const params = castParams(data)
+    const update = await handleGetRates(params);
 
-    let update: any;
+    // Update the parent document with the rates result
+    handleUpdateDocument(change.after, update);
 
-    /**
-     * Fetch Rates
-     */
-    try {
-      logs.fetchingRates();
-      
-      const result = await shipEngine.getRatesWithShipmentDetails(params);
-      update = { result };
-    } catch (error) {
-      logs.errorFetchRates(error as Error);
-      return;
-    }
+    logs.complete();
 
-    logs.ratesFetched([]); // remove args
-
-    /**
-     * Update Reference
-     */
-    try {
-      logs.parentUpdating();
-      change.after.ref.update(update);
-      logs.parentUpdated()
-    } catch (err) {
-      logs.errorUpdatingParent(err as Error);
-      return;
-    }
+    return;
   }
 )
+
+const castParams = (data: InputPayload): RequestPayload => {
+  // Include carrier ids from config
+  return {
+    shipment: data[config.shipmentKey] as RequestPayload['shipment'],
+    rateOptions: {
+      carrierIds: config.carrierIds
+    } as RequestPayload['rateOptions']
+  }
+}
+
+const handleGetRates = async (params: RequestPayload): Promise<UpdatePayload> => {
+  logs.fetchingRates(params);
+  try {  
+    const result = await shipEngine.getRatesWithShipmentDetails(params) as ResponsePayload;
+    logs.ratesFetched(result);
+    return { [config.ratesKey]: result };
+  } catch (error) {
+    logs.errorFetchRates(error as Error);
+    throw error;
+  }
+}
+
+const handleUpdateDocument = (after: DocumentSnapshot, update: UpdatePayload): void => {
+  logs.parentUpdating(update);
+  
+  try {
+    after.ref.update(update);
+    logs.parentUpdated()
+  } catch (err) {
+    logs.errorUpdatingParent(err as Error);
+    throw err;
+  }
+}
