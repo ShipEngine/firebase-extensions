ae692697b2cebde090844d7794a338cf70952b36
Patrick Jones <patrick.jones@auctane.com>
2021-09-22 12:22:29 -0500

    clean up rates config

--- extensions/rates/functions/src/config.ts
+++ extensions/rates/functions/src/config.ts
@@ -1,9 +1,12 @@
+const splitAndTrimString = (value: string): string[] =>
+  value.split(',').map((id) => id.trim());
+
 export default {
   location: process.env.LOCATION!,
   databaseInstance: process.env.SELECTED_DATABASE_INSTANCE!,
   shipEngineApiKey: process.env.SHIPENGINE_API_KEY!,
   collectionPath: process.env.COLLECTION_PATH!,
-  carrierIds: process.env.CARRIER_IDS!.split(',').map((id) => id.trim()),
+  carrierIds: splitAndTrimString(process.env.CARRIER_IDS!),
   shipmentKey: process.env.SHIPMENT_KEY!,
   ratesKey: process.env.RATES_KEY!,
 };
