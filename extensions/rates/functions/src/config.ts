7a872af14ff7cc3a5f63d4f1ae5416b81122a5aa
Patrick Jones <patrick.jones@auctane.com>
2021-09-22 15:28:54 -0500

    womp womp

--- extensions/rates/functions/src/config.ts
+++ extensions/rates/functions/src/config.ts
@@ -1,12 +1,9 @@
-const splitAndTrimString = (value: string): string[] =>
-  value.split(',').map((id) => id.trim());
-
 export default {
   location: process.env.LOCATION!,
   databaseInstance: process.env.SELECTED_DATABASE_INSTANCE!,
   shipEngineApiKey: process.env.SHIPENGINE_API_KEY!,
   collectionPath: process.env.COLLECTION_PATH!,
-  carrierIds: splitAndTrimString(process.env.CARRIER_IDS!),
+  carrierIds: process.env.CARRIER_IDS!.split(',').map((id) => id.trim()),
   shipmentKey: process.env.SHIPMENT_KEY!,
   ratesKey: process.env.RATES_KEY!,
 };
