24e583b5e941defc074543444a51ddebae893928
Patrick Jones <patrick.jones@auctane.com>
2021-09-23 03:51:13 -0500

    webhook types

--- extensions/track-label/functions/src/types.ts
+++ extensions/track-label/functions/src/types.ts
@@ -7,3 +7,36 @@ export type ResponsePayload =
 export type InputPayload = Record<string, Record<string, string>>;
 export type UpdatePayload = Record<string, ResponsePayload>;
 export type ParamSchema = { [key: string]: any };
+
+export type WebhookEvent =
+  | 'batch'
+  | 'carrier_connected'
+  | 'order_source_refresh_complete'
+  | 'rate'
+  | 'report_complete'
+  | 'sales_orders_imported'
+  | 'track';
+type Url = string;
+
+export interface CreateWebhookParams {
+  event: WebhookEvent;
+  url: Url;
+}
+
+export interface CreateWebhookResponse {
+  webhook_id: string;
+  url: Url;
+  event: WebhookEvent;
+}
+
+export interface Webhook {
+  webhookId: string;
+  url: Url;
+  event: WebhookEvent;
+}
+
+export type ListWebhooksResponse = Array<{
+  webhook_id: string;
+  url: Url;
+  event: WebhookEvent;
+}>;
