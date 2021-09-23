f48431b3a514124f6502868409d2cfa85805e0b4
Patrick Jones <patrick.jones@auctane.com>
2021-09-23 05:02:17 -0500

    add handleTrackingWebhook https handler

--- extensions/track-label/functions/src/index.ts
+++ extensions/track-label/functions/src/index.ts
@@ -1,46 +1,81 @@
 import ShipEngine from 'shipengine';
 import * as functions from 'firebase-functions';
 import { Change } from 'firebase-functions';
-import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+import * as admin from 'firebase-admin';
+
+import { DocumentSnapshot, DocumentData } from '@google-cloud/firestore';
 import {
   handleUpdateDocument,
   hasInputChanged,
+  mapDataToSchema,
 } from 'shipengine-firebase-common';
 
-import {
-  RequestPayload,
-  ResponsePayload,
-  InputPayload,
-  UpdatePayload,
-} from './types';
+import { camelizeKeys } from 'humps';
+
+import { RequestPayload, ResponsePayload, UpdatePayload } from './types';
 import config from './config';
 import logs from './logs';
-import { Request } from 'firebase-functions';
+import { TrackUsingCarrierCodeAndTrackingNumberTypes } from 'shipengine/esm/track-using-carrier-code-and-tracking-number';
+
+export type ParamSchema = { [key: string]: any };
 
 // Initialize ShipEngine client
 const shipEngine = new ShipEngine(config.shipEngineApiKey);
 
 logs.init(config);
 
-export const handleWebhookEvents = functions.handler.https.onRequest(
-  async (req: Request, resp) => {
-    // check if valid webhook
+/**
+ * __Webhook Handler__
+ * Throws any webhook requests into
+ */
+export const handleTrackingWebhook = functions.handler.https.onRequest(
+  async (req, res) => {
+    try {
+      const trackingEvent = camelizeKeys(
+        req.body()
+      ) as any as TrackUsingCarrierCodeAndTrackingNumberTypes.Result;
+      const app = admin.initializeApp();
+      const db = app.firestore();
+      await db
+        .collection(config.collectionPath)
+        .doc(`${trackingEvent.trackingNumber}/${config.trackingResultKey}`)
+        .set(trackingEvent, { merge: true });
+    } catch (error) {
+      throw error;
+    }
+    res.end(200);
   }
 );
 
 export const trackLabel = functions.handler.firestore.document.onWrite(
   async (change: Change<DocumentSnapshot>): Promise<void> => {
+    const inputSchema: ParamSchema = JSON.parse(config.inputSchema);
+    const castParams = (data: object) => mapDataToSchema(data, inputSchema);
+
     if (hasInputChanged(change, castParams)) {
-      let data = change.after.data() as InputPayload;
+      try {
+        const data: DocumentData = change.after.data() || {};
 
-      logs.start(data);
+        if (hasTrackingData(data)) return; // Tracking data already exists
 
-      // Build the request payload and execute the label purchase
-      const params = castParams(data);
-      const update = await handleGetLabelTrackingUpdate(params);
+        logs.start(data);
 
-      // Update the parent document with the tracking result
-      handleUpdateDocument(change.after, update);
+        // Build the request payload and execute the label purchase
+        const params: RequestPayload = mapDataToSchema(data, inputSchema);
+        const update = await handleGetLabelTrackingUpdate(params);
+
+        // Update the parent document with the tracking result
+        handleUpdateDocument(change.after, update);
+      } catch (err) {
+        // Update the document with error information on failure
+        if ((err as Error).message) {
+          handleUpdateDocument(change.after, {
+            [config.trackingResultKey]: {
+              errors: (err as Error).message,
+            },
+          });
+        }
+      }
 
       logs.complete();
     }
@@ -49,23 +84,24 @@ export const trackLabel = functions.handler.firestore.document.onWrite(
   }
 );
 
-const castParams = (data: InputPayload): RequestPayload => {
-  const label = data[config.labelKey];
-  // format input payload to fit request payload
-  return {
-    trackingNumber: label[config.trackingNumberKey],
-    carrierCode: label[config.carrierCodeKey],
-  };
+const hasTrackingData = (data: DocumentData): boolean => {
+  return (
+    data[config.trackingResultKey] !== undefined &&
+    data[config.trackingResultKey].errors === undefined
+  );
 };
 
 const handleGetLabelTrackingUpdate = async (
   params: RequestPayload
 ): Promise<UpdatePayload> => {
   logs.fetchingLabelTrackingData(params);
+
   try {
-    const result = (await shipEngine.trackUsingCarrierCodeAndTrackingNumber(
-      params
-    )) as ResponsePayload;
+    const result: ResponsePayload =
+      (await shipEngine.trackUsingCarrierCodeAndTrackingNumber(
+        params
+      )) as ResponsePayload;
+
     logs.labelTrackingDataFetched(result);
     return {
       [config.trackingResultKey]: result,
