3814612c836ac78e83eff5a080c352c58c75ea98
Andy Hite <andrew.hite@auctane.com>
2021-09-27 10:39:20 -0500

    Support rateId or shipment for purchasing labels

--- extensions/purchase-label/functions/src/types.ts
+++ extensions/purchase-label/functions/src/types.ts
@@ -1,6 +1,9 @@
+import { CreateLabelFromRateTypes } from 'shipengine/esm/create-label-from-rate';
 import { CreateLabelFromShipmentDetailsTypes } from 'shipengine/esm/create-label-from-shipment-details';
 
-export type RequestPayload = CreateLabelFromShipmentDetailsTypes.Params;
-export type ResponsePayload = CreateLabelFromShipmentDetailsTypes.Result;
+export type RequestPayload = CreateLabelFromRateTypes.Params &
+  CreateLabelFromShipmentDetailsTypes.Params;
+export type ResponsePayload = CreateLabelFromRateTypes.Result &
+  CreateLabelFromShipmentDetailsTypes.Result;
 export type UpdatePayload = { [key: string]: ResponsePayload };
 export type ParamSchema = { [key: string]: any };
