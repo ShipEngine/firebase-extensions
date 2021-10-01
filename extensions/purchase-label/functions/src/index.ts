a2acf846d4e9bb774db9ade79ea6151e543cdb7d
Salakar <mike.diarmid@gmail.com>
2021-10-01 12:09:50 +0100

    fix: await `handleUpdateDocument` promises

--- extensions/purchase-label/functions/src/index.ts
+++ extensions/purchase-label/functions/src/index.ts
@@ -40,11 +40,11 @@ export const purchaseLabel = functions.handler.firestore.document.onWrite(
         const update = await handlePurchaseLabel(params);
 
         // Update the parent document with the label data
-        handleUpdateDocument(change.after, update);
+        await handleUpdateDocument(change.after, update);
       } catch (err) {
         // Update the document with error information on failure
         if ((err as Error).message) {
-          handleUpdateDocument(change.after, {
+          await handleUpdateDocument(change.after, {
             [config.shippingLabelKey]: {
               errors: (err as Error).message,
             },
